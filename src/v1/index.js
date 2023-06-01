ce87a103c19bb53eade5e34690db56542511a610
Horacaz <xistir97@gmail.com>
2023-06-01 18:15:04 -0300

    Implementation of dotenv.

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const v1ClubTeamsRouter = require("./routes/clubTeams");
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
